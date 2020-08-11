b8bea441bf4d284fd5d4255cef6f4de2f7703e90
muchlist <whois.muchlis@gmail.com>
2020-08-11 10:55:57 +0700

    init

---
+++ utils/requester.py
@@ -0,0 +1,57 @@
+import requests
+
+
+class Postman(object):
+    params_get = {}
+    params_post = {}
+    base_url = ""
+
+    def __init__(self):
+        self.params_get = {
+            "key": "gulugulugulu",
+            "branch": "BANJARMASIN"
+        }
+        self.params_post = {
+            "key": "gulugulugulu"
+        }
+        self.base_url = "http://localhost:5001"
+
+    def set_base_url(self, base_url: str):
+        self.base_url = base_url
+
+    def set_param_post(self, params: dict):
+        self.params_post = params
+
+    def set_param_get(self, params: dict):
+        self.params_get = params
+
+    def get_ip_list(self) -> list:
+        url = f"{self.base_url}/api/cctv-ip"
+        x = requests.get(url, params=self.params_get, timeout=2.50)
+        if x.status_code == 200:
+            json_response = x.json()
+            return json_response["cctv_ip"]
+        else:
+            return []
+
+    def post_ip_status(self, cctv_list: list, code: int) -> str:
+        url = f"{self.base_url}/api/cctv-states-update"
+        body = {
+            "ip_addresses": cctv_list,
+            "ping_code": code
+        }
+        x = requests.post(url, json=body, params=self.params_post)
+        if x.status_code == 200:
+            return x.text
+        else:
+            return "Error"
+
+
+if __name__ == '__main__':
+    postman = Postman()
+    ip_list = postman.get_ip_list()
+    print(f"kumpulan ip nya {ip_list}")
+    if len(ip_list) == 0:
+        print("cctv kosong")
+    response = postman.post_ip_status(ip_list, 0)
+    print(response)
